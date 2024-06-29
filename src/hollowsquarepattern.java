a66c4c90c82018f25621edd36db8b76411673a19
keerthiVa5an <keerthijr5@gmail.com>
2024-06-29 09:23:41 +0530

    [UPDATE]Java(PatternPrinting):Delete hollow square pattern

--- src/hollowsquarepattern.java
+++
@@ -1,9 +0,0 @@
-public class hollowsquarepattern {
-    public static void main(String[] args){
-        int numberOfRows=4;
-        int i,j;
-        for (i = 1; i <= numberOfRows; i++) {
-
-        }
-    }
-}
